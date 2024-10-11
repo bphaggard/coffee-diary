3b0a2bae8088f4491e609ddf9d553f57f9a609c7
Patrik Mlcoch <patrik.mlcoch@gmail.com>
2024-10-11 19:08:08 +0200

    PhotoPicker u DetailScreen pro výběr fotky kávy

--- app/src/main/java/com/example/coffeediary/screens/DetailScreen.kt
+++ app/src/main/java/com/example/coffeediary/screens/DetailScreen.kt
@@ -1,6 +1,10 @@
 package com.example.coffeediary.screens
 
 import android.app.Application
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -17,12 +21,17 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.AddAPhoto
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -36,6 +45,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,6 +55,8 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import coil.compose.AsyncImage
 import com.example.coffeediary.R
+import com.example.coffeediary.bounceClick
+import com.example.coffeediary.navigation.Screen
 import com.example.coffeediary.parts.RatingBar
 import com.example.coffeediary.parts.titleToImageMap
 import com.example.coffeediary.ui.theme.CoffeeDiaryTheme
@@ -61,6 +73,12 @@ fun DetailScreen(
 
 ) {
     val coffeeId = viewModel.getCoffeeById(itemId).collectAsState(initial = null).value
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.PickVisualMedia(),
+        onResult = { uri -> selectedImageUri = uri }
+    )
 
     Scaffold(
         topBar = {
@@ -77,7 +95,22 @@ fun DetailScreen(
                         Icon(Icons.AutoMirrored.Filled.ArrowBack, "backIcon")
                     }
                 },
-                colors = TopAppBarDefaults.topAppBarColors(Color.Transparent)
+                colors = TopAppBarDefaults.topAppBarColors(Color.Transparent),
+                actions = {
+                    IconButton(
+                        colors = IconButtonDefaults.iconButtonColors(Color.Transparent),
+                        onClick = {
+                            singlePhotoPickerLauncher.launch(
+                                PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                            )
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Rounded.AddAPhoto,
+                            contentDescription = "add button"
+                        )
+                    }
+                }
             )
         }, content = {innerPadding ->
             Column(
@@ -102,10 +135,15 @@ fun DetailScreen(
                         verticalArrangement = Arrangement.Center
                     ) {
                         AsyncImage(
-                            model = imageResource,
-                            modifier = Modifier
-                                .fillMaxSize(0.85f),
-                            contentDescription = null)
+                            model = if(
+                                selectedImageUri != null
+                                ) { selectedImageUri } else imageResource,
+                            modifier = Modifier.fillMaxSize(0.85f),
+                            contentScale = if (
+                                selectedImageUri != null
+                                ) { ContentScale.Crop } else ContentScale.Fit ,
+                            contentDescription = null
+                        )
                     }
                 }
                 Spacer(modifier = Modifier.padding(15.dp))
